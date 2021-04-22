98b19e60f57c83336327435217e5a5edeaf564b4
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 08:56:01 +0800

    Edit user component

---
+++ src/app/components/edit-user/edit-user.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-user',
+  templateUrl: './edit-user.component.html',
+  styleUrls: ['./edit-user.component.css']
+})
+export class EditUserComponent implements OnInit {
+  user: any;
+  submitted = false;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getUser(id): void {
+    this.userService.getUser(id)
+      .subscribe(
+        data => {
+          this.user = data;
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
+
+  updateUser(): void {
+    const data = {
+      name: this.user.name,
+      surname: this.user.surname,
+      age: this.user.age
+    };
+
+    this.userService.updateUser(this.user.id, data)
+      .subscribe(
+        response => {
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
+
+}
