a34c874db22169bddf6c1d32c550a6fb3b072ec2
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 08:55:05 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddUserComponent,
+    UserListComponent,
+    EditUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
