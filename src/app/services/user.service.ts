fabcb99c9772e68789863d2de5ee2e51b54a799c
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 08:55:24 +0800

    User services

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8000/api/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getList(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  saveUser(data): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  getUser(id): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  updateUser(id, data):Observable<any> {
+    return this.http.post(`${baseUrl}/${id}`, data);
+  }
+}
