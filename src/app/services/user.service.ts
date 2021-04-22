9850ce85a3a61e7950b03c357a4f91f43f19413b
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 16:02:40 +0800

    Set environment

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8000/api/users';
+const baseUrl = 'http://localhost:3000/api/users';
 
 @Injectable({
   providedIn: 'root'
